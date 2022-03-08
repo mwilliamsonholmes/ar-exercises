9030846677b5d358597771f8a2ed45e02fd7630e
Melissa <mawilliamson10@gmail.com>
2022-03-08 02:38:38 +0000

    count number of stores generating > M annually

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "total revenue= $#{total_revenue}"
+average_revenue = Store.average(:annual_revenue)
+puts "average revenue of a store= $#{average_revenue}"
+
+high_earners = Store.where("annual_revenue > 1000000").count
+puts "number of high earner stores = #{high_earners}"
